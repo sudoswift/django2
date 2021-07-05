00fb6975a1aa2c6aeb7c7a61bab4ab3b71d73808
sudoswift <sudoswift@gmail.com>
2021-07-06 01:25:11 +0900

    add CreateView

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList,TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 app_name = 'base'
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create/', TaskCreate.as_view(), name='create')
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from .models import Task
 from django.shortcuts import render
 from django.http import HttpResponse, request
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class TaskList(ListView):
     model = Task #class based view에서 model을 models.py에서 설정한 모델이름으로 설정해주면 그 해당 모델을 이 view에서 쓸 수 있다.
@@ -11,3 +12,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('base:tasks') #석세스유알엘은 특정 행동을 한 후 어떤 페이지로 이동할 것인지 지정해주는 속성이다. reverse와 reverselazy 함수는 urls.py에서 appname을 지정해줘야 사용할 수 있다. 이 name을 지정하면 urls.py가 자동으로 해당 url로 매핑해준다. view의 generic을 사용할때는 reverse가 아닌 reverselazy를 사용해야한다.
