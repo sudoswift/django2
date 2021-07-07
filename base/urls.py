b8148f638fe8999cdce5001f5231e5e55aed2e3a
sudoswift <sudoswift@gmail.com>
2021-07-08 01:48:15 +0900

    add updateview

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 app_name = 'base'
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('create/', TaskCreate.as_view(), name='create')
+    path('create/', TaskCreate.as_view(), name='create'),
+    path('update/<int:pk>/', TaskUpdate.as_view(), name = 'update'),
 ]
