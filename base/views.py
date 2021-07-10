a72ce748008b96561b7b5bbc3065251e18600c25
sudoswift <sudoswift@gmail.com>
2021-07-10 15:42:56 +0900

    commit check

--- base/views.py
+++ base/views.py
@@ -18,7 +18,11 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task #class based view에서 model을 models.py에서 설정한 모델이름으로 설정해주면 그 해당 모델을 이 view에서 쓸 수 있다.
     context_object_name = 'tasks' # 원래 이 views.py에서 contextobjectname을 지정해주지 않으면 html에서 objectlist로 호출해야 하는데, contextobjectname을 지정해주면 html에서 이 지정한 이름을 호출할 수 있다.
     template_name = 'base/tasks.html'
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
@@ -26,13 +30,16 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('base:tasks') #석세스유알엘은 특정 행동을 한 후 어떤 페이지로 이동할 것인지 지정해주는 속성이다. reverse와 reverselazy 함수는 urls.py에서 appname을 지정해줘야 사용할 수 있다. 이 name을 지정하면 urls.py가 자동으로 해당 url로 매핑해준다. view의 generic을 사용할때는 reverse가 아닌 reverselazy를 사용해야한다. 경로를 appname까지 정확하게 맞춰줘야 reverselazy가 작동한다.
     template_name = 'base/create.html'
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('base:tasks')
     template_name = 'base/create.html'
 
